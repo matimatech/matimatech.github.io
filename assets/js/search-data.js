6332089928500959ced871a82a89b62d1d1f8896
matimatech <matimatechz@gmail.com>
2026-02-12 11:28:24 +0000

    Deploying to gh-pages from @ matimatech/matimatech.github.io@b3e5057466e4aa2d440be331010d6013f4d9b1f3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -52,6 +52,17 @@ ninja.data = [{
             window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
           
         },
+      },{id: "post-learn-marketing-analytics",
+        
+          title: "learn marketing analytics",
+        
+        description: "march &amp; april, looking forward to summer",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2015/testing/";
+          
+        },
       },{id: "post-a-post-with-formatting-and-links",
         
           title: "a post with formatting and links",
